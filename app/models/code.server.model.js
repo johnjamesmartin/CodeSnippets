8a89d005fa874087c8c9517a18d9c64475cbdcff
“John <johnjamesmartin@live.com>
2016-09-11 22:00:51 +0100

    Ready for deployment

--- app/models/code.server.model.js
+++ app/models/code.server.model.js
@@ -10,10 +10,16 @@ var mongoose = require('mongoose'),
  * Code Schema
  */
 var CodeSchema = new Schema({
-	name: {
+	description: {
 		type: String,
 		default: '',
-		required: 'Please fill Code name',
+		required: 'Please enter a code description',
+		trim: true
+	},
+	code: {
+		type: String,
+		default: '',
+		required: 'Please enter a code snippet',
 		trim: true
 	},
 	created: {
